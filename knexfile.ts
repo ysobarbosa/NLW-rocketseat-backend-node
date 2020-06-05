30465897511f1230832349cfac2456acb9ec7c9e
Yasmin <ysobarbosa@gmail.com>
2020-06-05 01:26:34 -0300

    Intalação de dependencias de banco de dados, criação de migrations iniciais

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+// Mesmo sendo ts, não pode usar sintaxe do tss, porque o knex ainda não suporta a linguagem
+import path from 'path';
+
+module.exports = { 
+
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    }
+};
