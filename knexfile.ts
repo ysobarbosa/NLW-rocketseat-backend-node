2f105441f224014c9f822f10194c76975baaf780
Yasmin <ysobarbosa@gmail.com>
2020-06-05 21:39:32 -0300

    Refatoração de código, criação de controllers, seeds, melhora de migrations, criação de métodos de filtragem, adição e leitura. Finalização do back end

--- knexfile.ts
+++ knexfile.ts
@@ -9,5 +9,9 @@ module.exports = {
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    }
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+    useNullAsDefault: true,
 };
--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -1,3 +1,18 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {
+    // Criar a tabela
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+    
+
+}
+
+export async function down(knex: Knex) {
+    // voltar atras (deletar tabela)
+    return knex.schema.dropTable('items');
+
+}
